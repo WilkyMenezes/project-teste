8e95eab783b3545296e8d8a4d72815c1e41f2015
WilkyMenezes <willkymenezes@outlook.com>
2022-04-05 21:49:07 -0300

    criando deploy heroko

--- models/database/index.js
+++ models/database/index.js
@@ -1,9 +1,18 @@
+const { ssl } = require("pg/lib/defaults");
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-  host:  process.env.DB_HOST,
-  port: 5432,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  ssl: true,
+  protocol: "postgres",
+  longging: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
 
 module.exports = sequelize;
